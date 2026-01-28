c93ff4f2f6c02217e82b6c830fb643c1169f9d64
oifikidine <oifikidine@gmail.com>
2026-01-28 16:33:31 +0300

    progression du 28 janvier

--- app.js
+++ app.js
@@ -1,30 +1,78 @@
-//Ici, nous créons notre application ExpressJs
+// Ici, nous créons notre application ExpressJs
 
 // j'importe le framework Expressjs
 const express = require('express');
 
-//j'initialise une application express
+// j'initialise une application express
 const app = express();
 
-// je définis la route par défault par exmple: localhost:3000
+// Middleware pour parser le JSON
+app.use(express.json());
 
-
+// Middleware généraux
 app.use((req,res,next) => {
     console.log("je suis un middleware dans le serveur");
     next();
 });
 
-app.use((req,res) => {
-    res.json({message: "Vous êtes bien sur le serveur Ndovoo"});
-    
-    
+// j'utilise un middleware 3 pour afficher le message dans la console
+app.use((req,res,next) => {
+    console.log("Bonjour, je suis le serveur!");
+    next();
 });
 
+// j'ajoute un middleware qui gère la sécurité d'accès 
+app.use((req,res,next) => {
+    // je permet l'accès à mon API depuis n'importe quel origine
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // j'autorise certaines en-têtes dans les requête reçues dans notre API. les en-tête autorisées sont  'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    // j'autorise d'envoyer des requêtes avec les méthodes : 'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
-// j'utilise un middleware 3 pour afficher le message dans la console
-app.use((req,res) => {
-    res.console("Bonjour, je suis le serveur!");
-    
+/* je dessine mes route avec Express
+*avec la méthode get
+route complète localhost:3200/api/fruit
+*/
+app.get('/api/fruit', (req,res,) => {
+    console.log("je passe dans la route /api/fruit");
+    // mon fruit
+    const fruit= [
+        {
+            id: 1,
+            nom: "pomme",
+            description:"fruit saisonier riche en vitamine C",
+            prix: 3
+        },
+
+        {
+            id: 2,
+            nom:"banane",
+            description:"fruit disponible toutes les saisons riche en vitamine C",
+            prix:2
+        }
+    ];
+
+    // en terme de réponse, je renvoie le tableau de fruits
+    res.status(200).json(fruit);
+});
+
+// Route POST pour créer un fruit
+app.post('/api/fruit', (req, res) => {
+    console.log("Je passe dans l'API /api/fruit");
+    res.status(201).json({
+        message: 'je suis dans post !'
+    });
+});
+
+// Route PUT pour modifier un fruit
+app.put('/api/fruit', (req, res) => {
+    console.log("PUT /api/fruits reçu !");
+    res.status(200).json({
+        message: 'je suis dans put !'
+    });
 });
 
 /*
